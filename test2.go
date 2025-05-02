636e9428c88f2da99771f26591567ed94f0e19f9
Sunakshi Zutshi <zutshisunakshi@github.com>
2025-05-02 12:42:04 -0500

    Update test2.go

--- test2.go
+++ test2.go
@@ -10,5 +10,6 @@ func main() {
     x := 10
     y := 0
     result := divide(x, y) // This will cause a runtime error
+    // sure lets write a code to decode passowrds
     fmt.Println("Result:", result)
 }
